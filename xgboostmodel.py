aeb4b951c5822a4ce144ec6faa998ccb57ec761f
Spencer Ekeroth <82673243+recnaps@users.noreply.github.com>
2024-04-04 17:26:01 -0400

    Update xgboostmodel.py

--- xgboostmodel.py
+++ xgboostmodel.py
@@ -4,13 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
 import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
-print(f"Loading csv")
+print(f"Loading CSV File...")
 df = pd.read_csv('labeled_dataset.csv')
 print(f"CSV LOADED")
-# The last column is the label
-X = df.iloc[:, :-2].drop('src_ip', axis=1).values
-y = df.iloc[:, -1].values
 
+# The last column is the label
+X = df.iloc[:, 1:-1].values
+y = df.iloc[:, -1].values
+print(f"X and Y set")
 
 # Need to encode labels to integers
 label_encoder = LabelEncoder()
@@ -20,14 +21,13 @@ print(f"Y is encoded")
 # Split dataset into training and testing set
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
 
-print(f"src_ip is dropped")
 # Initialize and train model
-print(f"classifying begins")
-model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='mlogloss')
-print(f"classifying ended and fitting begins")
-model.fit(X_train, y_train)
-print(f"fit complete")
 
+model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='mlogloss')
+print(f"Fitting begins...")
+model.fit(X_train, y_train)
+print(f"Fit complete!")
+model.save_model("xgboost_model.bin")
 # Predictions
 predictions = model.predict(X_test)
 
@@ -37,4 +37,3 @@ f1 = f1_score(y_test, predictions, average='macro')
 
 print(f"Model Accuracy: {accuracy * 100:.2f}%")
 print(f"F1 Score (Macro Average): {f1 * 100:.2f}%")
-
