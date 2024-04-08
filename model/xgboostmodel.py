d6c593fe27c007c7589a9f80ed30a6cb48ac44ac
jeremy-neale <jneale118@gmail.com>
2024-04-08 17:20:14 -0400

    xgboost now accepts sys args and optional test size argv, model stored in model/json

--- model/xgboostmodel.py
+++ model/xgboostmodel.py
@@ -1,13 +1,50 @@
 import pandas as pd
 import numpy as np
+import sys
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
 import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
-print(f"Loading CSV File...")
-df = pd.read_csv('labeled_dataset.csv')
-print(f"CSV LOADED")
 
+
+test_size_decimal = 0.2
+usage_message = "Usage: python3 [this_script].py [csv filename wth .csv] [model name with no extension] OPTIONAL:[decimal for test split size, [0 - 1)]"
+
+# Check if the correct number of arguments are provided
+if len(sys.argv) != 3 and len(sys.argv) != 4:
+    print(usage_message)
+    print("Note: only provide the file name, not the path. The csv file must be located in ../data/csv")
+    sys.exit(1)  # Exit with error code 1
+elif len(sys.argv) == 4:
+    try:
+        test_size_decimal = float(sys.argv[3])
+        if not 0 <= test_size_decimal < 1:
+            print(usage_message)
+            print("Test split size must be a decimal value between 0 (inclusive) and 1")
+            sys.exit(1)
+    except ValueError:
+        print(usage_message)
+        print("Invalid test split size. Please provide a decimal value between 0 and 1")
+        sys.exit(1)
+
+
+print(f"Loading CSV File: {sys.argv[1]}")
+try:
+    df = pd.read_csv(f"../data/csv/{sys.argv[1]}")
+    print(f"CSV file loaded")
+except FileNotFoundError:
+    print(usage_message)
+    print(f"ERROR: File '{sys.argv[1]}' not found")
+    sys.exit(1)
+except Exception as e:
+    print(usage_message)
+    print(f"ERROR: {e}")
+    sys.exit(1)
+
+print(f"CSV file loaded. Creating X/Y splits and training/testing splits")
+
+
+# mark the byte ranges to remove to avoid data leakage from overfitting source IP
 ipv4_source_start, ipv4_source_end = 97, 129
 ipv4_destination_start, ipv4_destination_end = 130, 160
 ipv4_identification_start, ipv4_identification_end = 33, 48
@@ -38,23 +75,24 @@ print(f"X and Y set")
 
 # Need to encode labels to integers
 label_encoder = LabelEncoder()
-y_encoded = label_encoder.fit_transform(y)
+Y_encoded = label_encoder.fit_transform(y)
 print(f"Y is encoded")
 
 # Split dataset into training and testing set
-X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y_encoded, test_size=test_size_decimal, random_state=42)
 # Initialize and train model
 model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='mlogloss')
 print(f"Fitting begins...")
-model.fit(X_train, y_train)
+model.fit(X_train, Y_train)
 print(f"Fit complete!")
-model.save_model("xgboost_model.json")
+model.save_model("json/" + sys.argv[2] + ".json")
 # Predictions
+print("Testing model with test size of " + str(test_size_decimal))
 predictions = model.predict(X_test)
 
 # Evaluation
-accuracy = accuracy_score(y_test, predictions)
-f1 = f1_score(y_test, predictions, average='macro')
+accuracy = accuracY_score(Y_test, predictions)
+f1 = f1_score(Y_test, predictions, average='macro')
 
 print(f"Model Accuracy: {accuracy}")
 print(f"F1 Score (Macro Average): {f1}")
