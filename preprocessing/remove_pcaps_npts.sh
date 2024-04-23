ec04b52ac72a79f7f76feacdabecaf6aa77cedad
jeremy-neale <jneale118@gmail.com>
2024-04-22 23:13:08 -0400

    added -rf flags

--- preprocessing/remove_pcaps_npts.sh
+++ preprocessing/remove_pcaps_npts.sh
@@ -3,5 +3,5 @@
 # simple script to remove pcap and npt files after the csv has been created
 
 # Remove the directories
-# rm -rf ../data/pcap/pcap_os_split/
-# rm -rf ../data/npt/
+rm -rf ../data/pcap/pcap_os_split/
+rm -rf ../data/npt/
